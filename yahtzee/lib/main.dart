b48c426ea81090db47af78318e36f55d185078d4
ouoxii <yuchuner317@gmail.com>
2024-04-22 18:04:30 +0800

    initial game

--- yahtzee/lib/main.dart
+++ yahtzee/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'dart:math';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:yahtzee/player.dart';
 
 void main() {
   runApp(MyApp());
-  // chiikawa usagi = chiikawa.forname('usagi');
-  // print(usagi.power * 2);
 }
 
 class MyApp extends StatelessWidget {
@@ -29,6 +29,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Player player1 = Player(name: 'usagi');
+  List<bool> isSelected = List.generate(20, (_) => false);
+
   final _diceEmojis = [
     '⚀',
     '⚁',
@@ -37,24 +40,123 @@ class _MyHomePageState extends State<MyHomePage> {
     '⚄',
     '⚅',
   ];
-  var _diceIndex = 0;
+  List<int> _diceIndex = [0, 0, 0, 0, 0];
+  List<String> categories = [
+    'Ones',
+    'Twos',
+    'Threes',
+    'Fours',
+    'Fives',
+    'Sixes'
+  ];
+  List<String> categories2 = [
+    'Three_of_a_kind',
+    'Four_of_a_kind',
+    'Small_Straight',
+    'Large_Straight',
+    'Yahtzee',
+    'Chance',
+    'Full_House',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Yahtzee"),
       ),
-      body: Center(
+      body: Container(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text(
-              "Yahtzee",
-              style: GoogleFonts.shadowsIntoLight(
-                fontSize: 30,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  player1.name,
+                  style: GoogleFonts.shadowsIntoLight(
+                    fontSize: 30,
+                  ),
+                ),
+                Text('SCORE:' + player1.score.toString()),
+              ],
+            ),
+            Row(
+                //目前記錄
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 350,
+                      margin: EdgeInsets.only(left: 100),
+                      child: ListView.builder(
+                        itemCount: categories.length,
+                        itemBuilder: (context, index) {
+                          String category = categories[index];
+                          int score = player1.getScore(category);
+                          return ListTile(
+                            title: Text('$category: $score'),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 350,
+                      child: ListView.builder(
+                        itemCount: categories2.length,
+                        itemBuilder: (context, index) {
+                          String category = categories2[index];
+                          int score = player1.getScore(category);
+                          return ListTile(
+                            title: Text('$category: $score'),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ]),
+            Container(
+              height: 100,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(
+                    _diceIndex.length,
+                    (index) => Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10), // 添加水平间距
+                      child: Text(
+                        '${_diceEmojis[_diceIndex[index]]}',
+                        style: TextStyle(fontSize: 36),
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ),
-            Text("player1")
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      for (var i = 0; i < 5; i++) {
+                        _diceIndex[i] = Random().nextInt(6);
+                      }
+                    });
+                  },
+                  child: const Text('Roll Dice'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      //將骰子的值存入player
+                    });
+                  },
+                  child: const Text('Play'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
--- yahtzee/lib/player.dart
+++ yahtzee/lib/player.dart
@@ -1,76 +1,39 @@
-class player {
+class Player {
   String name;
-  int score;
-  int? Ones;
-  int? Twos;
-  int? Threes;
-  int? Fours;
-  int? Fives;
-  int? Sixes;
-  int? Three_of_a_kind;
-  int? Four_of_a_kind;
-  int? Small_Straight;
-  int? Large_Straight;
-  int? Yahtzee;
-  int? Chance;
-  int? Full_House;
-  player(
-      {required this.name,
-      this.score = 0,
-      this.Ones,
-      this.Twos,
-      this.Threes,
-      this.Fours,
-      this.Fives,
-      this.Sixes,
-      this.Three_of_a_kind,
-      this.Four_of_a_kind,
-      this.Small_Straight,
-      this.Large_Straight,
-      this.Yahtzee,
-      this.Chance,
-      this.Full_House});
+  int score = 0;
+  List<String> categories = [
+    'Ones',
+    'Twos',
+    'Threes',
+    'Fours',
+    'Fives',
+    'Sixes'
+  ];
+  Map<String, int> scores = {
+    'Ones': 0,
+    'Twos': 0,
+    'Threes': 0,
+    'Fours': 0,
+    'Fives': 0,
+    'Sixes': 0,
+    'Three_of_a_kind': 0,
+    'Four_of_a_kind': 0,
+    'Small_Straight': 0,
+    'Large_Straight': 0,
+    'Yahtzee': 0,
+    'Chance': 0,
+    'Full_House': 0,
+  };
+
+  Player({required this.name});
+
   void setScore(String category, int value) {
-    switch (category) {
-      case 'Ones':
-        Ones = value;
-        break;
-      case 'Twos':
-        Twos = value;
-        break;
-      case 'Threes':
-        Threes = value;
-        break;
-      case 'Fours':
-        Fours = value;
-        break;
-      case 'Fives':
-        Fives = value;
-        break;
-      case 'Sixes':
-        Sixes = value;
-        break;
-      case 'Three_of_a_kind':
-        Three_of_a_kind = value;
-        break;
-      case 'Four_of_a_kind':
-        Four_of_a_kind = value;
-        break;
-      case 'Small_Straight':
-        Small_Straight = value;
-        break;
-      case 'Large_Straight':
-        Large_Straight = value;
-        break;
-      case 'Yahtzee':
-        Yahtzee = value;
-        break;
-      case 'Chance':
-        Chance = value;
-        break;
-      case 'Full_House':
-        Full_House = value;
-        break;
+    if (scores.containsKey(category)) {
+      scores[category] = value;
     }
+  }
+
+  int getScore(String category) {
+    return scores[category] ?? 0;
   }
 }
